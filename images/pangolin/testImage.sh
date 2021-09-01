8de8c8ed6ee68c36cd92cc7e38e3bf4fe8121a49
Sam Proctor <sp31@sanger.ac.uk>
2021-09-01 09:36:28 +0000

    Update test script for pangolin

--- images/pangolin/testImage.sh
+++ images/pangolin/testImage.sh
@@ -1,1 +1,6 @@
-docker run pangolin:latest
+docker run pangolin:latest --env DATE_PARTITION=21-09-01 \
+ --env SEQ_BATCH_FILE= \
+ --env SEQ_CONSENSUS_BATCH_FILE= \
+ --env SEQ_KEY_FILE= \
+ --env HERON_SAMPLES_BUCKET= \
+ --env HERON_SEQUENCES_TABLE= 
