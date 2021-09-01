2b999274480df427b1026371f8f5be7958c32cc4
Sam Proctor <sp31@sanger.ac.uk>
2021-09-01 09:14:45 +0000

    Update build file

--- images/pangolin/buildDeploy.sh
+++ images/pangolin/buildDeploy.sh
@@ -1,7 +1,11 @@
 
-# Log into AWS ECR
-aws ecr get-login-password --region eu-west-1 | sudo docker login --username AWS --password-stdin 889562587392.dkr.ecr.eu-west-1.amazonaws.com
+
+
 
 # Build the image and push it to AWS ECR
-docker build --no-cache --tag 889562587392.dkr.ecr.eu-west-1.amazonaws.com/pangolin:latest .
-sudo docker push 889562587392.dkr.ecr.eu-west-1.amazonaws.com/pangolin:latest
+# docker build --no-cache -t pangolin:latest .
+
+# Log into AWS ECR
+aws ecr get-login-password --region eu-west-1 | sudo docker login --username AWS --password-stdin 889562587392.dkr.ecr.eu-west-1.amazonaws.com
+# docker build --tag 889562587392.dkr.ecr.eu-west-1.amazonaws.com/pangolin:latest .
+# sudo docker push 889562587392.dkr.ecr.eu-west-1.amazonaws.com/pangolin:latest
