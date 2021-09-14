52b7acf7a38910e36801c585d368b22d7fee6386
Sam Proctor <sam.proctor@proctor-analytics.com>
2021-09-14 12:39:52 +0100

    Add choice task to determine to execute or finish the pipeline depending on LQP MetaData

--- heronPipeline/src/HeronPipeline/HeronPipelineStack.cs
+++ heronPipeline/src/HeronPipeline/HeronPipelineStack.cs
@@ -378,6 +378,70 @@ namespace HeronPipeline
 
 
             // +++++++++++++++++++++++++++++++++++++++++++++
+            // Task definition for adding squences to queue
+            // +++++++++++++++++++++++++++++++++++++++++++++
+
+            var addSequencesToQueueImage = ContainerImage.FromAsset("src/images/addSequencesToQueue");
+            var addSequencesToQueueTaskDefinition = new TaskDefinition(this, "addSequencesToQueueTaskDefinition", new TaskDefinitionProps{
+                Family = "addSequencesToQueue",
+                Cpu = "256",
+                MemoryMiB = "512",
+                NetworkMode = NetworkMode.AWS_VPC,
+                Compatibility = Compatibility.FARGATE,
+                ExecutionRole = ecsExecutionRole,
+                TaskRole = ecsExecutionRole
+            });
+            addSequencesToQueueTaskDefinition.AddContainer("addSequencesToQueueContainer", new Amazon.CDK.AWS.ECS.ContainerDefinitionOptions
+            {
+                Image = addSequencesToQueueImage,
+                Logging = new AwsLogDriver(new AwsLogDriverProps
+                {
+                    StreamPrefix = "addSequencesToQueue",
+                    LogGroup = new LogGroup(this, "addSequencesToQueueLogGroup", new LogGroupProps
+                    {
+                        LogGroupName = "addSequencesToQueueLogGroup",
+                        Retention = RetentionDays.ONE_WEEK,
+                        RemovalPolicy = RemovalPolicy.DESTROY
+                    })
+                }),
+                EntryPoint = new string[] { "python", "/home/app/app.py" }
+            });
+            var addSequencesToQueueContainer = addSequencesToQueueTaskDefinition.FindContainer("addSequencesToQueueContainer");
+
+            var addSequencesToQueueTask = new EcsRunTask(this, "addSequencesToQueueTask", new EcsRunTaskProps
+            {
+                IntegrationPattern = IntegrationPattern.RUN_JOB,
+                Cluster = cluster,
+                TaskDefinition = addSequencesToQueueTaskDefinition,
+                AssignPublicIp = true,
+                LaunchTarget = new EcsFargateLaunchTarget(),
+                ContainerOverrides = new ContainerOverride[] {
+                    new ContainerOverride {
+                        ContainerDefinition = addSequencesToQueueContainer,
+                        Environment = new TaskEnvironmentVariable[] {
+                            new TaskEnvironmentVariable{
+                                Name = "EXECUTION_MODE",
+                                Value = JsonPath.StringAt("$.executionMode")
+                             },
+                            new TaskEnvironmentVariable{
+                                Name = "HERON_SEQUENCES_TABLE",
+                                Value = sequencesTable.TableName
+                            },
+                            new TaskEnvironmentVariable{
+                                Name = "HERON_PROCESSING_QUEUE",
+                                Value = reprocessingQueue.QueueName
+                            },
+                            new TaskEnvironmentVariable{
+                              Name = "HERON_DAILY_PROCESSING_QUEUE",
+                              Value = dailyProcessingQueue.QueueName
+                            }
+                        }
+                    }
+                },
+                ResultPath = JsonPath.DISCARD
+            });
+
+            // +++++++++++++++++++++++++++++++++++++++++++++
             // +++++++++++++++++++++++++++++++++++++++++++++
             // ++++++++++++ Lambda Functions +++++++++++++++
             // +++++++++++++++++++++++++++++++++++++++++++++
@@ -497,8 +561,19 @@ namespace HeronPipeline
             // +++++++++++++++++++++++++++++++++++++++++++++
             // +++++++ Heron Pipeline State Machine ++++++++
             // +++++++++++++++++++++++++++++++++++++++++++++
+            var pipelineFinishTask = new Succeed(this, "pipelineSucceedTask");
+            var metaDataPresentCondition = Condition.BooleanEquals(JsonPath.StringAt("checkForMetaData.metaDataReady"), true);
+            var metaDataNotPresentCondition = Condition.BooleanEquals(JsonPath.StringAt("checkForMetaData.metaDataReady"), false);
+
+            var metaDataReadyChoiceTask = new Choice(this, "metaDataReadyChoiceTask", new ChoiceProps{
+                Comment = "Is LQP metadata available?"
+            });
+            metaDataReadyChoiceTask.When(metaDataPresentCondition, next: addSequencesToQueueTask);
+            metaDataReadyChoiceTask.When(metaDataNotPresentCondition, next: pipelineFinishTask);
+
             var pipelineChain = Chain
-                    .Start(checkLqpMetaDataIsPresentTask);
+                    .Start(checkLqpMetaDataIsPresentTask)
+                    .Next(metaDataReadyChoiceTask);
 
             var pipelineStateMachine = new StateMachine(this, "pipelineStateMachine", new StateMachineProps
             {
