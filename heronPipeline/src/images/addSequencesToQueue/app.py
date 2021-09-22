1af3d83062fd2db87caa0105aec52778e42afb12
Sam Proctor <sp31@sanger.ac.uk>
2021-09-22 09:22:12 +0000

    Update dynamoDB scan to catch sequenes that have been aligned but not yet called

--- heronPipeline/src/images/addSequencesToQueue/app.py
+++ heronPipeline/src/images/addSequencesToQueue/app.py
@@ -53,7 +53,7 @@ def main():
   if executionMode == "REPROCESS":
     queueName = reprocessingQueueName
   else:
-    scan_kwargs['FilterExpression'] = Attr("processingState").eq("consensus") | Attr("pangoCalled").eq('false') | Attr("lqpCalled").eq('false')
+    scan_kwargs['FilterExpression'] = Attr("processingState").eq("consensus") | Attr("pangoCalled").exists('false') | Attr("lqpCalled").exists('false') | Attr("genotypeVariant").exists('false')
   
   # Create the queue object
   queue = sqs.Queue(queueName)
