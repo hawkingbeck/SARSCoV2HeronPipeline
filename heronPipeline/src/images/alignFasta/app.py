42290c76ac2acb15baf2d0a4ecc6af4d12dabf75
Sam Proctor <sp31@sanger.ac.uk>
2021-10-09 18:52:38 +0000

    Update path selection for messageS3Key

--- heronPipeline/src/images/alignFasta/app.py
+++ heronPipeline/src/images/alignFasta/app.py
@@ -43,8 +43,15 @@ bucket = s3.Bucket(bucketName)
 dynamodb = boto3.resource('dynamodb', region_name="eu-west-1", config=config)
 sequencesTable = dynamodb.Table(heronSequencesTableName)
 
+sampleLocalFilename = "/tmp/sample.fasta"
+consensusLocalFilename = "/tmp/consensus.fa"
 referenceFastaLocalFilename = "/tmp/ref.fa"
+mappedSamFastaLocalFilename = "/tmp/sample.mapped.sam"
+alignedLocalFilename = "/tmp/aligned.fa"
+
 messageListLocalFilename = "/tmp/messageList.json"
+
+
 bucket.download_file(referenceFastaPrefix, referenceFastaLocalFilename)
 bucket.download_file(messageListS3Key, messageListLocalFilename)
 
@@ -53,8 +60,37 @@ messageList = json.load(messageListLocalFilename)
 # Iterate over each item in the message list and align the sample
 for message in messageList:
    print(f"Message: {message.consensusFastaPath}")
+   # Download the consensus fasta
+   consensusFastaKey = message.consensusFastaPath
 
+   try:
+      bucket.download_file(consensusFastaKey, sampleLocalFilename)
+   except:
+      print(f"File not found: {consensusFastaKey}")
 
+   with open(sampleLocalFilename, 'r') as file:
+      data = file.read()
+
+   sample = json.loads(data)
+
+   consensusFasta = sample['consensus']
+
+   with open(consensusLocalFilename, 'w') as file:
+      file.write(consensusFasta)
+   
+   ##############################################
+   # Step 1. 
+   ##############################################
+    # Run minimap 
+      # -a:  output in sam
+      # -x asm5:  asm-to-ref mapping, for ~0.1% sequence divergence
+   subprocess.run(
+      "./minimap2 -t minimap2_threads -a -x asm5 {} {} > {}".format(
+         referenceFastaLocalFilename, consensusLocalFilename, mappedSamFastaLocalFilename
+      ),
+      check=True,
+      shell=True
+   )
 # Open and iterate over the file
 # seqConsensusFile
 # sequences = list()
