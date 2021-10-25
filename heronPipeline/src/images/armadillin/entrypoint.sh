f79474a37328593b77ca93155e2751d1697ce742
Sam Proctor <sp31@sanger.ac.uk>
2021-10-25 09:34:54 +0000

    Print the head of the armadillin results

--- heronPipeline/src/images/armadillin/entrypoint.sh
+++ heronPipeline/src/images/armadillin/entrypoint.sh
@@ -5,4 +5,5 @@
 SEQ_BATCH_FILE=$SEQ_DATA_ROOT/$DATE_PARTITION/seqBatchFiles/sequences_$ITERATION_UUID.fasta
 gzip -c $SEQ_BATCH_FILE > /tmp/seqFile.gz
 armadillin /tmp/seqFile.gz > /tmp/results.tsv
+head /tmp/results.tsv
 python3 app.py
