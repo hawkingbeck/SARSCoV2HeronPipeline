996d2ce4d201ee6d9a797cc552f25b8bfa0a71d8
Sam Proctor <sp31@sanger.ac.uk>
2021-11-09 11:36:13 +0000

    Break out Armadillin task

--- heronPipeline/src/HeronPipeline/ArmadillinModel.cs
+++ heronPipeline/src/HeronPipeline/ArmadillinModel.cs
@@ -16,3 +16,154 @@ using Amazon.CDK.AWS.DynamoDB;
 using Amazon.CDK.AWS.SQS;
 using Stack = Amazon.CDK.Stack;
 using Queue = Amazon.CDK.AWS.SQS.Queue;
+
+
+namespace HeronPipeline {
+  internal sealed class ArmadillinModel: Construct {
+    public EcsRunTask armadillinTask;
+    public EcsRunTask armadillinTestTask;
+    private Construct scope;
+    private Role ecsExecutionRole;
+    private Amazon.CDK.AWS.ECS.Volume volume;
+    private Cluster cluster;
+    private Bucket bucket;
+    private Table sequencesTable;
+    private RetryProps retryItem;
+    private TaskDefinition armadillinTaskDefinition;
+    private Amazon.CDK.AWS.ECS.ContainerDefinition armadillinContainer;
+
+    public ArmadillinModel(Construct scope, string id, Role executionRole, Amazon.CDK.AWS.ECS.Volume volume, Cluster cluster, Bucket bucket, Table sequencesTable): base(scope, id)
+    {
+      this.scope = scope;
+      this.ecsExecutionRole = executionRole;
+      this.volume = volume;
+      this.cluster = cluster;
+      this.bucket = bucket;
+      this.sequencesTable = sequencesTable;
+      this.retryItem = new RetryProps{
+        BackoffRate = 5,
+        Interval = Duration.Seconds(2),
+        MaxAttempts = 3,
+        Errors = new string[] {"STATES.ALL"}
+      };
+    }
+
+    public void Create()
+    {
+      var armadillinImage = ContainerImage.FromAsset("src/images/armadillin", new AssetImageProps
+      { 
+      });
+      this.armadillinTaskDefinition = new TaskDefinition(this, "armadillinTaskDefinition", new TaskDefinitionProps{
+          Family = "armadillin",
+          Cpu = "1024",
+          MemoryMiB = "4096",
+          NetworkMode = NetworkMode.AWS_VPC,
+          Compatibility = Compatibility.FARGATE,
+          ExecutionRole = ecsExecutionRole,
+          TaskRole = ecsExecutionRole,
+          Volumes = new Amazon.CDK.AWS.ECS.Volume[] { volume }
+      });
+      this.armadillinTaskDefinition.AddContainer("armadillinContainer", new Amazon.CDK.AWS.ECS.ContainerDefinitionOptions
+      {
+          Image = armadillinImage,
+          Logging = new AwsLogDriver(new AwsLogDriverProps
+          {
+              StreamPrefix = "armadillin",
+              LogGroup = new LogGroup(this, "armadillinLogGroup", new LogGroupProps
+              {
+                  LogGroupName = "armadillinLogGroup2",
+                  Retention = RetentionDays.ONE_WEEK,
+                  RemovalPolicy = RemovalPolicy.DESTROY
+              })
+          })
+      });
+      this.armadillinContainer = this.armadillinTaskDefinition.FindContainer("armadillinContainer");
+      armadillinContainer.AddMountPoints(new MountPoint[] {
+              new MountPoint {
+                  SourceVolume = "efsVolume",
+                  ContainerPath = "/mnt/efs0",
+                  ReadOnly = false,
+              }
+          });
+      this.armadillinTask = new EcsRunTask(this, "armadillinPlaceTask", new EcsRunTaskProps
+      {
+          IntegrationPattern = IntegrationPattern.RUN_JOB,
+          Cluster = cluster,
+          TaskDefinition = this.armadillinTaskDefinition,
+          AssignPublicIp = true,
+          LaunchTarget = new EcsFargateLaunchTarget(),
+          ContainerOverrides = new ContainerOverride[] {
+              new ContainerOverride {
+                  ContainerDefinition = this.armadillinContainer,
+                  Environment = new TaskEnvironmentVariable[] {
+                      new TaskEnvironmentVariable{
+                        Name = "DATE_PARTITION",
+                        Value = JsonPath.StringAt("$.date")
+                      },
+                      new TaskEnvironmentVariable{
+                        Name = "MESSAGE_LIST_S3_KEY",
+                        Value = JsonPath.StringAt("$.sampleBatch.messageListS3Key")
+                      },
+                      new TaskEnvironmentVariable{
+                        Name = "HERON_SAMPLES_BUCKET",
+                        Value = bucket.BucketName
+                      },
+                      new TaskEnvironmentVariable{
+                        Name = "SEQ_DATA_ROOT",
+                        Value = "/mnt/efs0/seqData"
+                      },
+                      new TaskEnvironmentVariable{
+                        Name = "ITERATION_UUID",
+                        Value = JsonPath.StringAt("$.sampleBatch.iterationUUID")
+                      },
+                      new TaskEnvironmentVariable{
+                          Name = "HERON_SEQUENCES_TABLE",
+                          Value = sequencesTable.TableName
+                      }
+                  }
+              }
+          },
+          ResultPath = JsonPath.DISCARD
+      });
+    }
+
+    public void CreateTestTask(){
+      this.armadillinTestTask = new EcsRunTask(this, "armadillinPlaceTestTask", new EcsRunTaskProps
+      {
+          IntegrationPattern = IntegrationPattern.RUN_JOB,
+          Cluster = cluster,
+          TaskDefinition = this.armadillinTaskDefinition,
+          AssignPublicIp = true,
+          LaunchTarget = new EcsFargateLaunchTarget(),
+          ContainerOverrides = new ContainerOverride[] {
+              new ContainerOverride {
+                  ContainerDefinition = armadillinContainer,
+                  Environment = new TaskEnvironmentVariable[] {
+                      new TaskEnvironmentVariable{
+                        Name = "DATE_PARTITION",
+                        Value = JsonPath.StringAt("$.date")
+                      },
+                      new TaskEnvironmentVariable{
+                        Name = "HERON_SAMPLES_BUCKET",
+                        Value = bucket.BucketName
+                      },
+                      new TaskEnvironmentVariable{
+                        Name = "SEQ_DATA_ROOT",
+                        Value = "/mnt/efs0/seqData"
+                      },
+                      new TaskEnvironmentVariable{
+                        Name = "ITERATION_UUID",
+                        Value = JsonPath.StringAt("$.sampleBatch.iterationUUID")
+                      },
+                      new TaskEnvironmentVariable{
+                          Name = "HERON_SEQUENCES_TABLE",
+                          Value = sequencesTable.TableName
+                      }
+                  }
+              }
+          },
+          ResultPath = JsonPath.DISCARD
+      });
+    }
+  }
+}
