a95fa24511760e49d27a62c1213b3197ed3c1ded
Sam Proctor <sp31@sanger.ac.uk>
2021-11-10 09:32:31 +0000

    Update run config

--- heronPipeline/src/functions/getMessageCount/app.py
+++ heronPipeline/src/functions/getMessageCount/app.py
@@ -39,7 +39,7 @@ def lambda_handler(event, context):
     queue = sqs.Queue(queueName)
     queue.load()
 
-    sampleBatchSize = 1000
+    sampleBatchSize = 2000
     attributes = queue.attributes
     sequenceCount = int(attributes['ApproximateNumberOfMessages'])
     sequencesPerMapIteration = 40*sampleBatchSize
