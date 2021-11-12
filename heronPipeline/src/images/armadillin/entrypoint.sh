22eea6661fec0bcda68286ff4c20129df0a0650d
Sam Proctor <sp31@sanger.ac.uk>
2021-11-12 12:34:47 +0000

    Update armadillin to use aligned fasta files

--- heronPipeline/src/images/armadillin/entrypoint.sh
+++ heronPipeline/src/images/armadillin/entrypoint.sh
@@ -4,6 +4,6 @@
 
 SEQ_BATCH_FILE=$SEQ_DATA_ROOT/$DATE_PARTITION/seqBatchFiles/sequences_$ITERATION_UUID.fasta
 gzip -c $SEQ_BATCH_FILE > /tmp/seqFile.gz
-armadillin /tmp/seqFile.gz > /tmp/results.tsv
+armadillin --seqs_are_aligned /tmp/seqFile.gz > /tmp/results.tsv
 head /tmp/results.tsv
 python3 app.py
