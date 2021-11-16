a70bf5f669a22a2f8c0e08ee7b05130113e3257a
Sam Proctor <sp31@sanger.ac.uk>
2021-11-16 11:00:59 +0000

    Update pango image

--- heronPipeline/src/functions/readSampleBatchFromQueue/app.py
+++ heronPipeline/src/functions/readSampleBatchFromQueue/app.py
@@ -43,7 +43,7 @@ def lambda_handler(event, context):
     bucket = s3.Bucket(bucketName)
 
     dateString = event['date'] #os.getenv("DATE_PARTITION")
-    sampleBatchSize = event['sampleBatchSize']
+    sampleBatchSize = int(event['sampleBatchSize'])
 
 
     #++++++++++++++++++++++++++++++++++++++++++++
