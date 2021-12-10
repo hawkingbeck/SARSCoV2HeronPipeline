e9993afd8fa6b2e84f30de0cfb95c6b640673c52
Sam Proctor <Sam.proctor@proctor-analytics.com>
2021-12-10 22:28:39 +0000

    Delete bootstrap.sh

--- setup/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-export CDK_NEW_BOOTSTRAP=1
-
-npx cdk bootstrap aws://889562587392/eu-west-1 --profile heron aws://889562587392/eu-west-1
-
-# npx cdk bootstrap aws://889562587392/eu-west-1 --profile heron \
-#     --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess \
-#     aws://889562587392/eu-west-1
