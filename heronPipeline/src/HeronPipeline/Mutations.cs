3d4a39b2b5ec5c9fcc587c2f693eafbb40c2fd15
Sam Proctor <sp31@sanger.ac.uk>
2021-12-21 12:16:04 +0000

    Add test task to mutations

--- heronPipeline/src/HeronPipeline/Mutations.cs
+++ heronPipeline/src/HeronPipeline/Mutations.cs
@@ -22,6 +22,7 @@ namespace HeronPipeline
   internal sealed class MutationsModel: Construct 
   {
     public EcsRunTask mutationsTask;
+    public EcsRunTask mutationsTestTask;
     public Succeed skipMutationsTask;
     private Construct scope;
     private string id;
@@ -49,7 +50,11 @@ namespace HeronPipeline
       };
     }
 
-    public void Create()
+    public void Create(){
+      CreateTask();
+      CreateTest();
+    }
+    public void CreateTask()
     {
       var mutationsImage = ContainerImage.FromAsset("src/images/mutations");
       var mutationsTaskDefinition = new TaskDefinition(this, this.id + "_mutationsTaskDefinition", new TaskDefinitionProps{
@@ -136,6 +141,93 @@ namespace HeronPipeline
 
       skipMutationsTask = new Succeed(this, "skipMutationsTask");
 
+
+
+    }
+
+    private void CreateTest(){
+      var mutationsImage = ContainerImage.FromAsset("src/images/mutations");
+      var mutationsTaskDefinition = new TaskDefinition(this, this.id + "_mutationsTaskDefinition", new TaskDefinitionProps{
+          Family = this.id + "_mutations",
+          Cpu = "1024",
+          MemoryMiB = "2048",
+          NetworkMode = NetworkMode.AWS_VPC,
+          Compatibility = Compatibility.FARGATE,
+          ExecutionRole = ecsExecutionRole,
+          TaskRole = ecsExecutionRole,
+          Volumes = new Amazon.CDK.AWS.ECS.Volume[] { volume }
+      });
+      mutationsTaskDefinition.AddContainer("mutationsContainer", new Amazon.CDK.AWS.ECS.ContainerDefinitionOptions
+      {
+          Image = mutationsImage,
+          Logging = new AwsLogDriver(new AwsLogDriverProps
+          {
+              StreamPrefix = "mutationsVariants",
+              LogGroup = new LogGroup(this, "mutationsLogGroup", new LogGroupProps
+              {
+                  LogGroupName = this.id + "mutationsLogGroup",
+                  Retention = RetentionDays.ONE_WEEK,
+                  RemovalPolicy = RemovalPolicy.DESTROY
+              })
+          })
+      });
+      var mutationsContainer = mutationsTaskDefinition.FindContainer("mutationsContainer");
+      mutationsContainer.AddMountPoints(new MountPoint[] {
+              new MountPoint {
+                  SourceVolume = "efsVolume",
+                  ContainerPath = "/mnt/efs0",
+                  ReadOnly = false,
+              }
+          });
+      this.mutationsTestTask = new EcsRunTask(this, this.id + "_mutationsTestPlaceTask", new EcsRunTaskProps
+      {
+          IntegrationPattern = IntegrationPattern.RUN_JOB,
+          Cluster = cluster,
+          TaskDefinition = mutationsTaskDefinition,
+          AssignPublicIp = true,
+          LaunchTarget = new EcsFargateLaunchTarget(),
+          ContainerOverrides = new ContainerOverride[] {
+              new ContainerOverride {
+                  ContainerDefinition = mutationsContainer,
+                  Environment = new TaskEnvironmentVariable[] {
+                      new TaskEnvironmentVariable{
+                        Name = "DATE_PARTITION",
+                        Value = JsonPath.StringAt("$.date")
+                      },
+                      new TaskEnvironmentVariable{
+                        Name = "MESSAGE_LIST_S3_KEY",
+                        Value = JsonPath.StringAt("$.sampleBatch.messageListS3Key")
+                      },
+                      new TaskEnvironmentVariable{
+                        Name = "HERON_SAMPLES_BUCKET",
+                        Value = bucket.BucketName
+                      },
+                      new TaskEnvironmentVariable{
+                        Name = "SEQ_DATA_ROOT",
+                        Value = "/mnt/efs0/seqData"
+                      },
+                      new TaskEnvironmentVariable{
+                        Name = "ITERATION_UUID",
+                        Value = JsonPath.StringAt("$.sampleBatch.iterationUUID")
+                      },
+                      new TaskEnvironmentVariable{
+                          Name = "HERON_SEQUENCES_TABLE",
+                          Value = sequencesTable.TableName
+                      },
+                      new TaskEnvironmentVariable{
+                          Name = "REF_FASTA_KEY",
+                          Value = "resources/MN908947.fa"
+                      },
+                      new TaskEnvironmentVariable{
+                          Name = "REF_GB_KEY",
+                          Value = "resources/MN908947.gb"
+                      },
+                  }
+              }
+          },
+          ResultPath = JsonPath.DISCARD
+      });
     }
   }
+      
 }
