eebb1acdfcf21bcddfe0279e1226e020c711110d
Sam Proctor <sp31@sanger.ac.uk>
2022-01-07 10:39:17 +0000

    Update function

--- heronPipeline/src/functions/startTableExport/app.py
+++ heronPipeline/src/functions/startTableExport/app.py
@@ -1,9 +1,11 @@
 import os
 from sys import exit, stderr
+from datetime import datetime
 import boto3
 from botocore.exceptions import ClientError
 from botocore.config import Config
 from boto3.dynamodb.conditions import Key
+
 
 config = Config(
    retries = {
@@ -20,4 +22,20 @@ def lambda_handler(event, context):
   print(f"Bucket: {heronBucketName}")
   print(f"Table: {heronMutationsTableArn}")
 
-  return "Hello World"
+  exportDate = datetime(2022, 1, 7)
+  exportPartition = datetime.strftime(exportDate, "%Y-%m-%D")
+
+  # Create a DynamoDB Client
+  dynamodb = boto3.resource('dynamodb', region_name="eu-west-1", config=config)
+  ret = dynamodb.export_table_to_point_in_time(
+    TableArn=heronMutationsTableArn,
+    ExportTime=exportDate,
+    S3Bucket=heronBucketName,
+    S3Prefix=f"mutations/{exportPartition}",
+    S3SseAlgorithm='AES256'|'KMS',
+    S3SseKmsKeyId='string',
+    ExportFormat='DYNAMODB_JSON'
+  )
+  
+
+  return ret
