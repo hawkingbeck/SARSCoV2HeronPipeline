0e32a85ff2e56b5cd5cf4c179f6b0a45a446ca30
Sam Proctor <sam.proctor@proctor-analytics.com>
2022-01-24 10:38:35 +0000

    Update Program.cs to remove account ID

---
+++ heronPipeline/src/HeronPipeline/Program.cs
@@ -0,0 +1,40 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HeronPipeline
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            var pipeline = new HeronPipelineStack(app, "HeronProdStack", new StackProps
+            {
+                
+            });
+
+            Tags.Of(pipeline).Add("service-class", "prod");
+            Tags.Of(pipeline).Add("pi", "sp31@sanger.ac.uk");
+
+            var testPipeline = new HeronPipelineStack(app, "HeronTestStack", new StackProps
+            {
+                
+            });
+
+            Tags.Of(testPipeline).Add("service-class", "test");
+            Tags.Of(testPipeline).Add("pi", "sp31@sanger.ac.uk");
+
+            var devPipeline = new HeronPipelineStack(app, "HeronDevStack", new StackProps
+            {
+                
+            });
+
+            Tags.Of(devPipeline).Add("service-class", "dev");
+            Tags.Of(devPipeline).Add("pi", "sp31@sanger.ac.uk");
+
+            app.Synth();
+        }
+    }
+}
