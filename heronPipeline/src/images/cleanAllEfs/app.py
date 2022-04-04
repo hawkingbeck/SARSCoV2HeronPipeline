15584e963c87562de7bb6688a46641e271ce85d0
Sam Proctor <sam.proctor@proctor-analytics.com>
2022-04-04 09:30:03 +0100

    Add task to clean efs

--- heronPipeline/src/images/cleanAllEfs/app.py
+++ heronPipeline/src/images/cleanAllEfs/app.py
@@ -31,12 +31,12 @@ directoryList = os.listdir(sampleDataRoot)
 
 for directory in directoryList:
   directory = f"{sampleDataRoot}/{directory}"
-  print(f"Directory: {directory}")
+  print(f"Directory 1: {directory}")
   shutil.rmtree(directory, ignore_errors=False, onerror=None)
   
 directoryList = os.listdir(sampleDataRoot)
 
 for directory in directoryList:
-  print(f"Directory: {directory}")
+  print(f"Directory 2: {directory}")
 
 
